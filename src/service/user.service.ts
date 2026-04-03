e60ae93041bd2a1ae286f5094e2a9a5611e4482c
Rynarl <ryandranarlan@gmail.com>
2026-04-03 14:33:23 +0800

    docs: add jsdoc syntax to user.service.ts

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -5,6 +5,16 @@ import { hashPassword } from '../utils/password.util';
 import { BadRequestError } from '../utils/error';
 
 export class UserService {
+    /**
+     * Memproses registrasi pembuatan user baru ke dalam database.
+     * Fungsi ini akan melakukan validasi duplikasi username dan email terlebih dahulu,
+     * kemudian melakukan hashing pada password sebelum disimpan dengan aman.
+     * Mengembalikan data user yang baru dibuat tanpa menyertakan field password.
+     * 
+     * @param data Payload data user baru (username, email, password)
+     * @returns Objek response berisikan data profile user (tanpa password)
+     * @throws {BadRequestError} Jika username atau email sudah digunakan dalam sistem
+     */
     static async createUser(data: typeof users.$inferInsert) {
         // Cek duplikasi
         const existingUsers = await db.select().from(users).where(
